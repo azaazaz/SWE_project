fa72e82e65c712afc64baed58848743024c86c5e
AzerinUFAZ <azerinsalahova@gmail.com>
2025-10-13 11:03:26 +0400

    small change for pw4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-res.send("Hello, World!");
+res.send("Hello, Worldddddd!");
+
 });
 // This block now checks if the file is being run directly
 // before starting the server. This prevents the server from
